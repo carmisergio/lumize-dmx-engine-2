24562071c86f734f506017e1d708c0f4a65d711c
Sergio Carmine <me@sergiocarmi.net>
2022-09-17 21:59:28 +0200

    Put config file in /etc

--- src/configreader.h
+++ src/configreader.h
@@ -18,13 +18,13 @@
 #include "./logger.h"
 
 // Configuration file path
-#define CONFIG_FILE_PATH "./lumizedmxengine2.conf"
+#define CONFIG_FILE_PATH "/etc/lumizedmxengine2.conf"
 
 // Default config values
 #define DEFAULT_CONFIG_PORT 8056
 #define DEFAULT_CONFIG_CHANNELS 25
 #define DEFAULT_CONFIG_FPS 50
-#define DEFAULT_CONFIG_DEFAULT_TRANSITION 100 // ms
+#define DEFAULT_CONFIG_DEFAULT_TRANSITION 1000 // ms
 #define DEFUALT_CONFIG_LOG_DEBUG false
 
 // Configuration keys
